21ed1a528915ea27e58ee519151928b88d694a86
jaydeep17 <jaydp17@gmail.com>
2015-09-19 15:46:59 +0530

    add stetho and remove useless junk

---
+++ app/src/main/java/com/example/parth/contacts/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.parth.contacts;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+}
